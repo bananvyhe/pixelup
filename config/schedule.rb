444507d829616a682b5cf4c65a1a6e83f7c28658
Yuriy Kotohin <loadonden@yahoo.com>
2026-03-10 21:18:27 +0500

    shed fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,12 @@
 set :output, "log/whenever.log"
 set :environment, ENV.fetch("RAILS_ENV", "development")
 
-set :job_template, "/bin/zsh -lc ':job'"
+set :job_template, "/bin/bash -lc ':job'"
 
 job_type :rails_runner_in_app,
-         "export PATH=\"$HOME/.local/share/mise/shims:$HOME/.local/bin:$PATH\" && " \
+         "if [ -d \"$HOME/.local/share/mise/shims\" ]; then " \
+         "export PATH=\"$HOME/.local/share/mise/shims:$HOME/.local/bin:$PATH\"; " \
+         "fi; " \
          "cd :path && bundle exec rails runner -e :environment ':task' :output"
 
 every 1.hour do
