721e70eae894bd32650231b07ecb5ef4030bce08
Yuriy Kotohin <loadonden@yahoo.com>
2026-03-10 22:58:52 +0500

    shed fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,6 +4,9 @@ set :environment, ENV.fetch("RAILS_ENV", "development")
 set :job_template, "/bin/bash -lc ':job'"
 
 job_type :rails_runner_in_app,
+         "export BUNDLE_PATH=\"${BUNDLE_PATH:-/usr/local/bundle}\"; " \
+         "export GEM_HOME=\"${GEM_HOME:-/usr/local/bundle}\"; " \
+         "export PATH=\"/usr/local/bundle/bin:$PATH\"; " \
          "if [ -d \"$HOME/.local/share/mise/shims\" ]; then " \
          "export PATH=\"$HOME/.local/share/mise/shims:$HOME/.local/bin:$PATH\"; " \
          "fi; " \
