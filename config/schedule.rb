09c19803c863cac8e205d50e2400040e005c488c
Yuriy Kotohin <loadonden@yahoo.com>
2026-03-11 11:09:39 +0500

    prod env dep fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,9 @@ set :environment, ENV.fetch("RAILS_ENV", "development")
 
 set :job_template, "/bin/bash -lc ':job'"
 
+env :RAILS_MASTER_KEY, ENV["RAILS_MASTER_KEY"] if ENV["RAILS_MASTER_KEY"].present?
+env :JWT_SIGNING_KEY, ENV["JWT_SIGNING_KEY"] if ENV["JWT_SIGNING_KEY"].present?
+
 job_type :rails_runner_in_app,
          "export BUNDLE_PATH=\"${BUNDLE_PATH:-/usr/local/bundle}\"; " \
          "export GEM_HOME=\"${GEM_HOME:-/usr/local/bundle}\"; " \
