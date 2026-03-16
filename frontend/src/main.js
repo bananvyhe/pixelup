abaa49f5012f29fc0791aa42956f309ccc3e1405
Yuriy Kotohin <loadonden@yahoo.com>
2026-03-16 22:54:51 +0500

    metrics move to js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,6 +7,21 @@ import AdminPage from "./pages/AdminPage.vue"
 import vuetify from "./plugins/vuetify"
 import "./styles.css"
 
+if (import.meta.env.PROD) {
+  const gaId = "G-PPKP0L3CY7"
+  const gtagScript = document.createElement("script")
+  gtagScript.async = true
+  gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`
+  document.head.appendChild(gtagScript)
+
+  window.dataLayer = window.dataLayer || []
+  function gtag() {
+    window.dataLayer.push(arguments)
+  }
+  gtag("js", new Date())
+  gtag("config", gaId)
+}
+
 const routes = [
   { path: "/", redirect: "/login" },
   { path: "/login", component: LoginPage },
